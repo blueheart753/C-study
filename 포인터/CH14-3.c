fe12a74c2699d7ef6208a6de60bd0c400df24314
kimminam <bluemind8520@gmail.com>
2025-03-07 17:04:30 +0900

    3번 문제 해결

--- 포인터/CH14-3.c
+++ 포인터/CH14-3.c
@@ -4,12 +4,24 @@ int main(void)
 {
     int ar[50] = {0,}, N;
     int *p;
+    int cnt = 0;
 
     scanf("%d",&N);
-
     for(p = ar; p < ar+N; p++)
     {
         scanf("%d",p);
     }
 
+    for(p = ar; p < ar+N; p++)
+    {
+        if(*p != 0)
+        {
+            cnt++;
+        }
+        else {
+            break;
+        }
+    }
+
+    printf("%d",cnt);
 }
